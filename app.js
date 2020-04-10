a8f624ac9b4d6db056924d634a6ee3e7df07b84f
Harish Krishnan <harishkrishnan1993@gmail.com>
2020-04-10 12:04:25 +0530

    Added pug template to other views

--- app.js
+++ app.js
@@ -5,18 +5,21 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.set("view engine", "pug");
+app.set("views", "views");
+
 app.use(express.static(path.join(__dirname, "public")));
 
-const adminRoutes = require("./routes/admin");
+const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/admin", adminRoutes);
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-	res.sendFile(path.join(__dirname, "views", "add-product.html"));
+	res.status(404).render("404");
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -5,9 +5,11 @@ const express = require("express");
 const router = express.Router();
 
 const rootDir = require("../util/path");
+const adminData = require("./admin");
 
 router.get("/", (req, res, next) => {
-	res.sendFile(path.join(rootDir, "views", "shop.html"));
+	const products = adminData.products;
+	res.render("shop", { prods: products, docTitle: "Shop" });
 });
 
 module.exports = router;
