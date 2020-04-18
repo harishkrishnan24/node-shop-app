3a32d92c0638fdaa9a94d58fd9baa840c7eb1d32
Harish Krishnan <harishkrishnan1993@gmail.com>
2020-04-18 12:20:47 +0530

    Update product

--- controllers/admin.js
+++ controllers/admin.js
@@ -32,11 +32,8 @@ exports.getEditProduct = (req, res, next) => {
 		return res.redirect("/");
 	}
 	const prodId = req.params.productId;
-	req.user
-		.getProducts({ where: { id: prodId } })
-		// Product.findByPk(prodId)
-		.then((products) => {
-			const product = products[0];
+	Product.findById(prodId)
+		.then((product) => {
 			if (!product) {
 				return res.redirect("/");
 			}
@@ -56,24 +53,23 @@ exports.postEditProduct = (req, res, next) => {
 	const updatedPrice = req.body.price;
 	const updatedImageUrl = req.body.imageUrl;
 	const updatedDesc = req.body.description;
-	Product.findByPk(prodId)
-		.then((product) => {
-			product.title = updatedTitle;
-			product.price = updatedPrice;
-			product.description = updatedDesc;
-			product.imageUrl = updatedImageUrl;
-			return product.save();
-		})
+	const product = new Product(
+		updatedTitle,
+		updatedPrice,
+		updatedDesc,
+		updatedImageUrl,
+		prodId
+	);
+	product
+		.save()
 		.then((result) => {
-			console.log("UPDATED PRODUCT!");
 			res.redirect("/admin/products");
 		})
 		.catch((err) => console.log(err));
 };
 
 exports.getProducts = (req, res, next) => {
-	req.user
-		.getProducts()
+	Product.fetchAll()
 		.then((products) => {
 			res.render("admin/products", {
 				prods: products,
