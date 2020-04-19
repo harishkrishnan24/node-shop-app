3d61bdd383d7c0d4004a58530228f73a20098aff
Harish Krishnan <harishkrishnan1993@gmail.com>
2020-04-19 16:38:23 +0530

    Implement database for cart and orders

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,7 +13,14 @@ exports.postAddProduct = (req, res, next) => {
 	const imageUrl = req.body.imageUrl;
 	const price = req.body.price;
 	const description = req.body.description;
-	const product = new Product(title, price, description, imageUrl);
+	const product = new Product(
+		title,
+		price,
+		description,
+		imageUrl,
+		null,
+		req.user._id
+	);
 	product
 		.save()
 		.then((result) => {
@@ -82,11 +89,8 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
 	const prodId = req.body.productId;
-	Product.findByPk(prodId)
-		.then((product) => {
-			return product.destroy();
-		})
-		.then((result) => {
+	Product.deleteById(prodId)
+		.then(() => {
 			console.log("DESTROYED PRODUCT");
 			res.redirect("/admin/products");
 		})
--- models/product.js
+++ models/product.js
@@ -2,12 +2,13 @@ const getDb = require("../util/database").getDb;
 const mongodb = require("mongodb");
 
 class Product {
-	constructor(title, price, description, imageUrl, id) {
+	constructor(title, price, description, imageUrl, id, userId) {
 		this.title = title;
 		this.price = price;
 		this.description = description;
 		this.imageUrl = imageUrl;
-		this._id = new mongodb.ObjectId(id);
+		this._id = id ? new mongodb.ObjectId(id) : null;
+		this.userId = userId;
 	}
 
 	save() {
@@ -51,6 +52,19 @@ class Product {
 			.next()
 			.then((product) => {
 				return product;
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}
+
+	static deleteById(prodId) {
+		const db = getDb();
+		return db
+			.collection("products")
+			.deleteOne({ _id: new mongodb.ObjectId(prodId) })
+			.then((product) => {
+				console.log("Deleted");
 			})
 			.catch((err) => {
 				console.log(err);
