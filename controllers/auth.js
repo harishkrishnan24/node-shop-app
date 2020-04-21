928c098ac599c90e51b1e496af5b5d42e680357a
Harish Krishnan <harishkrishnan1993@gmail.com>
2020-04-21 20:07:44 +0530

    Add express session

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,14 @@
 exports.getLogin = (req, res, next) => {
+	// const isLoggedIn =
+	// 	req.get("Cookie").split(";")[1].trim().split("=")[1] === "true";
 	res.render("auth/login", {
 		pageTitle: "Login",
 		path: "/login",
+		isAuthenticated: false,
 	});
+};
+
+exports.postLogin = (req, res, next) => {
+	req.session.isLoggedIn = true;
+	res.redirect("/");
 };
