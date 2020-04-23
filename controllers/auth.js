81321a88e9f12c91cc72c1fdc3ee8cfa04b2147b
Harish Krishnan <harishkrishnan1993@gmail.com>
2020-04-23 16:26:18 +0530

    Add csrf token and auth middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -19,14 +19,29 @@ exports.getSignup = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-	User.findById("5e9d2f916ad48d602c4ed5d7")
+	const email = req.body.email;
+	const password = req.body.password;
+
+	User.findOne({ email })
 		.then((user) => {
-			req.session.isLoggedIn = true;
-			req.session.user = user;
-			req.session.save((err) => {
-				console.log(err);
-				res.redirect("/");
-			});
+			if (!user) {
+				return res.redirect("/login");
+			}
+			bcrypt
+				.compare(password, user.password)
+				.then((doMatch) => {
+					if (doMatch) {
+						req.session.isLoggedIn = true;
+						req.session.user = user;
+						return req.session.save((err) => {
+							console.log(err);
+							res.redirect("/");
+						});
+					} else {
+						res.redirect("/login");
+					}
+				})
+				.catch((err) => res.redirect("/login"));
 		})
 		.catch((err) => console.log(err));
 };
@@ -41,18 +56,19 @@ exports.postSignup = (req, res, next) => {
 			if (userDoc) {
 				return res.redirect("/signup");
 			}
-			return bcrypt.hash(password, 12);
-		})
-		.then((hashedPassword) => {
-			const user = new User({
-				email,
-				password: hashedPassword,
-				cart: { items: [] },
-			});
-			return user.save();
-		})
-		.then((result) => {
-			res.redirect("/login");
+			return bcrypt
+				.hash(password, 12)
+				.then((hashedPassword) => {
+					const user = new User({
+						email,
+						password: hashedPassword,
+						cart: { items: [] },
+					});
+					return user.save();
+				})
+				.then((result) => {
+					res.redirect("/login");
+				});
 		})
 		.catch((err) => {
 			console.log(err);
