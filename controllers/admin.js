4c1e134f059fe3e9c401290a42e2b62cab9f3a1b
Harish Krishnan <harishkrishnan1993@gmail.com>
2020-04-30 16:09:23 +0530

    invoice pdf creation

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,5 +1,6 @@
 const Product = require("../models/product");
 const { validationResult } = require("express-validator/check");
+const fileHelper = require("../util/file");
 
 exports.getAddProduct = (req, res, next) => {
 	res.render("admin/edit-product", {
@@ -122,6 +123,7 @@ exports.postEditProduct = (req, res, next) => {
 				return res.redirect("/");
 			}
 			if (image) {
+				fileHelper.deleteFile(product.imageUrl);
 				product.imageUrl = image.path;
 			}
 			product.title = updatedTitle;
@@ -159,7 +161,14 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
 	const prodId = req.body.productId;
-	Product.deleteOne({ _id: prodId, userId: req.user._id })
+	Product.findById(prodId)
+		.then((product) => {
+			if (!product) {
+				return next(new Error("Product not found!"));
+			}
+			fileHelper.deleteFile(product);
+			return Product.deleteOne({ _id: prodId, userId: req.user._id });
+		})
 		.then(() => {
 			console.log("DESTROYED PRODUCT");
 			res.redirect("/admin/products");
--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,8 @@
 const Product = require("../models/product");
 const Order = require("../models/order");
+const fs = require("fs");
+const path = require("path");
+const PDFDocument = require("pdfkit");
 
 exports.getProducts = (req, res, next) => {
 	Product.find()
@@ -140,4 +143,65 @@ exports.getOrders = (req, res, next) => {
 			error.httpStatusCode = 500;
 			return next(error);
 		});
+};
+
+exports.getInvoice = (req, res, next) => {
+	const orderId = req.params.orderId;
+	Order.findById(orderId)
+		.then((order) => {
+			if (!order) {
+				return next(new Error("No order found!"));
+			}
+			if (order.user.userId.toString() !== eq.user._id.toString()) {
+				return next(new Error("Unauthorized"));
+			}
+
+			const invoiceName = `invoice-${orderId}.pdf`;
+			const invoicePath = path.join("data", "invoices", invoiceName);
+
+			const pdfDoc = new PDFDocument();
+			res.setHeader("Content-Type", "application/pdf");
+			res.setHeader(
+				"Content-Disposition",
+				'inline; filename="' + invoiceName + '"'
+			);
+			pdfDoc.pipe(fs.createWriteStream(invoicePath));
+			pdfDoc.pipe(res);
+
+			pdfDoc.fontSize(26).text("Invoice", {
+				underline: true,
+			});
+			pdfDoc.text("-----------------------------------------");
+			let totalPrice = 0;
+			order.products.forEach((product) => {
+				totalPrice += product.quantity * product.product.price;
+				pdfDoc
+					.fontSize(14)
+					.text(
+						product.product.title +
+							" - " +
+							product.quantity +
+							" x " +
+							"$" +
+							product.product.price
+					);
+			});
+			pdfDoc.text("------------------------------------------");
+			pdfDoc.fontSize(20).text("Total Price: $" + totalPrice);
+			pdfDoc.end();
+
+			// fs.readFile(invoicePath, (err, data) => {
+			// 	if (err) {
+			// 		return next(err);
+			// 	}
+
+			// 	res.setHeader("Content-Type", "application/pdf");
+			// 	res.setHeader(
+			// 		"Content-Disposition",
+			// 		'inline; filename="' + invoiceName + '"'
+			// 	);
+			// 	res.send(data);
+			// });
+		})
+		.catch((err) => next(err));
 };
