1321e3ee379af50af3541c444689276182c797a3
Harish Krishnan <harishkrishnan1993@gmail.com>
2020-05-02 16:09:13 +0530

    Added pagination

--- controllers/shop.js
+++ controllers/shop.js
@@ -4,14 +4,31 @@ const fs = require("fs");
 const path = require("path");
 const PDFDocument = require("pdfkit");
 
+const ITEMS_PER_PAGE = 2;
+
 exports.getProducts = (req, res, next) => {
+	const page = +req.query.page || 1;
+	let totalItems;
+
 	Product.find()
+		.countDocuments()
+		.then((numProducts) => {
+			totalItems = numProducts;
+			return Product.find()
+				.skip((page - 1) * ITEMS_PER_PAGE)
+				.limit(ITEMS_PER_PAGE);
+		})
 		.then((products) => {
-			console.log(products);
 			res.render("shop/product-list", {
 				prods: products,
-				pageTitle: "All Products",
+				pageTitle: "Products",
 				path: "/products",
+				totalProducts: totalItems,
+				hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+				hasPrevious: page > 1,
+				nextPage: page + 1,
+				previousPage: page - 1,
+				lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
 			});
 		})
 		.catch((err) => {
@@ -39,12 +56,28 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
+	const page = +req.query.page || 1;
+	let totalItems;
+
 	Product.find()
+		.countDocuments()
+		.then((numProducts) => {
+			totalItems = numProducts;
+			return Product.find()
+				.skip((page - 1) * ITEMS_PER_PAGE)
+				.limit(ITEMS_PER_PAGE);
+		})
 		.then((products) => {
 			res.render("shop/index", {
 				prods: products,
 				pageTitle: "Shop",
 				path: "/",
+				totalProducts: totalItems,
+				hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+				hasPrevious: page > 1,
+				nextPage: page + 1,
+				previousPage: page - 1,
+				lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
 			});
 		})
 		.catch((err) => {
